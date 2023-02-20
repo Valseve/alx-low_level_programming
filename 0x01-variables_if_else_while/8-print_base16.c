1a0e3477b38a038b5148f2679831524fdf6036a5
Valseve <jalilbabzz@gmail.com>
2023-02-20 06:32:00 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	c = 'a';
+	while
+		(i < 10)
+		{
+			putchar(i);
+			i++;
+		}
+	while
+		(c <= 'f')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
