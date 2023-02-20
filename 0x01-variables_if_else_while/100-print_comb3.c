abcbe9f04fd1a3d8191851743b600c16a6bc186d
Valseve <jalilbabzz@gmail.com>
2023-02-20 07:34:30 -0800

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int a;
+
+	i = 0;
+	a = 1
+	while
+		(i < 10)
+		{
+			putchar(i + '0');
+			i++;
+			while (a < 10)
+			{
+				putchar(a + '0');
+				a++;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
