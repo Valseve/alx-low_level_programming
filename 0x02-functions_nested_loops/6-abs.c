a30d4d6eb95fc1bae6a6444cc52588e14bcbb906
Valseve <jalilbabzz@gmail.com>
2023-02-22 09:19:53 -0800

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,21 +3,17 @@
 /**
  * _abs - Entry point
  *
- * @int: The integer to check
+ * @i: The integer to check
  * Return: the absolute value of int
  */
-int _abs(int)
+int _abs(int i)
 {
-	int i, j;
-
-	i = 0;
-	j = 1
-	while (i <= '0')
+	if (i < 0)
 	{
-		return (i);
-		while (j = i * -1)
-		{
-			return (j);
-		}
+		int j;
+
+		j = i * -1;
+		return (j);
 	}
+	return (i);
 }
