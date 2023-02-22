3c294ed599ac69d173136add9f481f34a843f3f0
Valseve <jalilbabzz@gmail.com>
2023-02-22 08:46:58 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @i: The integer to check
+ * Return: the absolute value of int
+ */
+int _abs(int i)
+{
+	int i;
+
+	if (i <= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
