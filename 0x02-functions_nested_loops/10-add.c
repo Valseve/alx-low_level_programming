5324b5a242f42e1253efb4dab980a0beb385ff22
Valseve <jalilbabzz@gmail.com>
2023-02-22 12:15:38 -0800

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: int a is to be added
+ * @b: int b is to be added
+ * Return: The result
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
