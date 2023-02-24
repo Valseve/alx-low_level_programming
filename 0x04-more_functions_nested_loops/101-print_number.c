10166c6ae7c9b704f3789931d1415685224313c8
Valseve <jalilbabzz@gmail.com>
2023-02-24 12:43:03 -0800

    12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: Integer to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(n);
+	}
+	else
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
