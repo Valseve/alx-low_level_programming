b4d20350866cc450180a42870d9a03d9c979c99e
Valseve <jalilbabzz@gmail.com>
2023-02-24 09:01:40 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,20 @@ void more_numbers(void)
 {
 	int i, j;
 
-	j = '0';
+	j = 0;
 	while (j < 10)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+			if (i > 9)
+			{
+				_putchar('1');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
 		}
 		_putchar('\n');
 		j++;
