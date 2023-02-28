4cb90c37f0e442133bbd890955d6b1f067f3a9af
Valseve <jalilbabzz@gmail.com>
2023-02-28 08:49:47 -0800

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * c_string - function that reverses a string
+ *
+ * @s: string to be cersed
+ * Return: void
+ */
+void c_string(char *s)
+{
+	char c = s[0];
+	int a = 0;
+	int i;
+
+	while (s[a] != '\0')
+	a++;
+	for (i = 0; i < a; i++)
+	{
+		a--;
+		c = s[i];
+		s[i] = s[a];
+		s[a] = c;
+	}
+}
+
