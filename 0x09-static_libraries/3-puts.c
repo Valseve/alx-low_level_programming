918e2c3a09bdb8d978faff155d9066cddf403870
Valseve <jalilbabzz@gmail.com>
2023-03-10 12:43:22 -0800

    0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - Entry point
+ *
+ * @s: string
+ * Return: void
+ */
+
+void _puts(char *s)
+{
+	_puts(s);
+}
