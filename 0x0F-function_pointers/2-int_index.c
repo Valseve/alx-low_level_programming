4f5e5930e251431607a85c7f35f4a2223cb95089
Valseve <jalilbabzz@gmail.com>
2023-03-24 06:00:10 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * int_index - return index place if comparison = true, else -1
+ * int_index - function that searches for an integer
+ *
  * @array: array
  * @size: size of elements in array
  * @cmp: pointer to func of one of the 3 in main
